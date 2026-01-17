4b30baf96be5bed80eb7998e16fee3136b668fcb
Oran Shaharabany <orans@liveu.tv>
2026-01-17 22:07:10 +0200

    Fix compute variables: remove duplicates and invalid syntax

--- infra/modules/compute/variables.tf
+++ infra/modules/compute/variables.tf
@@ -1,10 +1,31 @@
-variable "instance_type" { type = string  default = "t3.micro" }
-variable "enable_ami_bonus" { type = bool  default = false }
-variable "name_prefix" { type = string }
-variable "subnet_id" { type = string }
-variable "security_group_id" { type = string }
-variable "instance_type" { type = string }
-variable "key_name" { type = string }
-variable "ubuntu_ami_ssm_path" { type = string }
-variable "enable_ami_bonus" { type = bool }
-variable "ubuntu_ami_ssm_path" { type = string  default = "/aws/service/canonical/ubuntu/server/22.04/stable/current/amd64/hvm/ebs-gp2/ami-id" }
+variable "name_prefix" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "security_group_id" {
+  type = string
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "ubuntu_ami_ssm_path" {
+  type    = string
+  default = "/aws/service/canonical/ubuntu/server/22.04/stable/current/amd64/hvm/ebs-gp2/ami-id"
+}
+
+variable "enable_ami_bonus" {
+  type    = bool
+  default = false
+}
+
